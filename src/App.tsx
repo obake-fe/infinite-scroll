b8755bfbb1757c714a71cb3af51ced3d642014fc
obake-fe <daichismash@gmail.com>
2021-08-02 18:56:21 +0900

    feat: write sample code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { List, WindowScroller } from 'react-virtualized';
+
+
+// List data as an array of strings
+const list = [
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn5',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn10',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn15',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn20',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn25',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn',
+    'Brian Vaughn30',
+];
+
+function rowRenderer({
+ key, // Unique key within array of rows
+ index, // Index of row within collection
+ isScrolling, // The List is currently being scrolled
+ isVisible, // This row is visible within the List (eg it is not an overscanned row)
+ style, // Style object to be applied to row (to position it)
+}: any) {
+    return (
+        <div key={key} style={style}>
+            {list[index]}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <WindowScroller>
+          {({ height, isScrolling, onChildScroll, scrollTop }) => (
+              <List
+                  width={300}
+                  height={height}
+                  autoHeight
+                  isScrolling={isScrolling}
+                  onScroll={onChildScroll}
+                  rowCount={list.length}
+                  rowHeight={100}
+                  rowRenderer={rowRenderer}
+                  scrollTop={scrollTop}
+              />
+          )}
+      </WindowScroller>
+    );
 }
 
 export default App;
