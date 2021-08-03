ea06ce88b91b36709dcc1963e80a0873cfad47c3
obake-fe <daichismash@gmail.com>
2021-08-03 12:59:05 +0900

    style: install material-ui and format style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { List, WindowScroller } from "react-virtualized";
+import {
+  createStyles,
+  makeStyles,
+  Card,
+  CardActionArea
+} from "@material-ui/core";
 
 // List data as an array of strings
 const list = [
@@ -35,34 +41,59 @@ const list = [
   "Brian Vaughn30"
 ];
 
-function rowRenderer({
-  key, // Unique key within array of rows
-  index, // Index of row within collection
-  isScrolling, // The List is currently being scrolled
-  isVisible, // This row is visible within the List (eg it is not an overscanned row)
-  style // Style object to be applied to row (to position it)
-}: any) {
-  return (
-    <div key={key} style={style}>
-      {list[index]}
-    </div>
-  );
-}
+const useStyles = makeStyles(() =>
+  createStyles({
+    card: {
+      textAlign: "center",
+      backgroundColor: "#aaa",
+      height: 170,
+      fontSize: 20
+    },
+    cardArea: {
+      marginTop: 10
+    },
+    cardButton: {
+      height: "100%"
+    }
+  })
+);
 
 function App() {
+  const classes = useStyles();
+
+  function rowRenderer({
+    key, // Unique key within array of rows
+    index, // Index of row within collection
+    isScrolling, // The List is currently being scrolled
+    isVisible, // This row is visible within the List (eg it is not an overscanned row)
+    style // Style object to be applied to row (to position it)
+  }: any) {
+    return (
+      <div key={key} style={style}>
+        <Card className={classes.card}>
+          <CardActionArea className={classes.cardButton}>
+            {list[index]}
+          </CardActionArea>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <WindowScroller>
-      {({ height, isScrolling, onChildScroll, scrollTop }) => (
-        <List
-          width={300}
-          height={height} // ビューポートのheightを設定している
-          autoHeight // WindowScrollerのときは必須感
-          isScrolling={isScrolling} // WindowScroller用 スクロール検知
-          rowCount={list.length} // 列の数
-          rowHeight={100} // 列の高さ(px)
-          rowRenderer={rowRenderer} // リストのDOM
-          scrollTop={scrollTop} // スクロール用オフセット値
-        />
+      {({ height, isScrolling, registerChild, onChildScroll, scrollTop }) => (
+        <div ref={registerChild} className={classes.cardArea}>
+          <List
+            width={300}
+            height={height} // ビューポートのheightを設定している
+            autoHeight // WindowScrollerのときは必須感
+            isScrolling={isScrolling} // WindowScroller用 スクロール検知
+            rowCount={list.length} // 列の数
+            rowHeight={200} // 列の高さ(px)
+            rowRenderer={rowRenderer} // リストのDOM
+            scrollTop={scrollTop} // スクロール用オフセット値
+          />
+        </div>
       )}
     </WindowScroller>
   );
